dade27c4fbcf860a176757d84fade676e48c7af4
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-05-17 09:04:41 +0100

    Add usePage import to PostEdit component for accessing page props

--- resources/js/pages/posts/edit.tsx
+++ resources/js/pages/posts/edit.tsx
@@ -1,7 +1,7 @@
 import { PlaceholderPattern } from '@/components/ui/placeholder-pattern';
 import AppLayout from '@/layouts/app-layout';
 import { type BreadcrumbItem } from '@/types';
-import { Head, Link, useForm } from '@inertiajs/react';
+import { Head, Link, useForm, usePage } from '@inertiajs/react';
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
