13c4406f5bfe4971dbf782cf9f89a1bd0db3da66
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-05-17 09:12:04 +0100

    Fix incorrect route in PostEdit component by changing from posts.store to posts.update for form submission

--- resources/js/pages/posts/edit.tsx
+++ resources/js/pages/posts/edit.tsx
@@ -26,7 +26,7 @@ export default function PostEdit() {
 
     const submit: FormEventHandler = (e: FormEvent<Element>) => {
         e.preventDefault();
-        put(route("posts.store"));
+        put(route("posts.update"));
     };
     
     return (
