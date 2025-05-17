1083c71bd1ce973d388ee918e692dab38dff1652
ShuaibuPassionateProgrammer <ibrahimshuaibuisa7@gmail.com>
2025-05-17 08:42:54 +0100

    Fix title spacing in Head component of Posts page

--- resources/js/pages/posts/index.tsx
+++ resources/js/pages/posts/index.tsx
@@ -15,7 +15,7 @@ export default function Posts() {
 
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
-            <Head title="Posts" />
+            <Head title="Posts " />
             <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
                 <div className="overflow-x-auto">
                     <Link 
